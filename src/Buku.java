06b590429a20a43a86fc8186965d2a1cd7f4ece4
Rifqi <rifqifebrianto746@gmail.com>
2023-11-27 18:22:52 +0700

    Tugas PBO Kelompok 3

---
+++ src/Buku.java
@@ -0,0 +1,6 @@
+public class Buku {
+    private int idBuku;
+    private String judul;
+    private String noISBN;
+    private String statusKetersediaan;
+}
---
+++ src/Notifikasi.java
@@ -0,0 +1,6 @@
+public class Notifikasi extends Buku{
+    private int idNotif;
+    private int noAnggota;
+    private String pesan;
+    private String timeStamp;
+}
