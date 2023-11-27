01cc929c6a182cbd2851d8775d2b4fb3f3a2ab20
Rifqi <rifqifebrianto746@gmail.com>
2023-11-27 23:24:54 +0700

    Tugas Kelompok 3

---
+++ src/anggotaPerpustakaan.java
@@ -0,0 +1,11 @@
+public class anggotaPerpustakaan {
+    private String nama;
+    private int noAnggota;
+    private String alamat;
+    private String riwayatPeminjaman;
+    public void viewRiwayatPeminjaman(){
+
+    }
+    public void mencariBuku(){}
+    public void terimaNotif(){}
+}
