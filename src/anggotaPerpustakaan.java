ac4c28983053fc0ef4293dd9b01fdbf35de78d54
rifqee23 <2257051008@students.unila.ac.id>
2023-11-28 13:12:53 +0700

    update class

--- src/anggotaPerpustakaan.java
+++ src/anggotaPerpustakaan.java
@@ -1,12 +1,75 @@
+import java.util.ArrayList;
+
 public class anggotaPerpustakaan {
         private String nama;
         private int noAnggota;
         private String alamat;
-        private String riwayatPeminjaman;
-        public void viewRiwayatPeminjaman(){
+        private ArrayList<transaksiPeminjaman> riwayatPeminjaman;
+
+        public anggotaPerpustakaan(String nama, int noAnggota, String alamat, ArrayList<transaksiPeminjaman> riwayatPeminjaman) {
+                this.nama = nama;
+                this.noAnggota = noAnggota;
+                this.alamat = alamat;
+                this.riwayatPeminjaman = riwayatPeminjaman;
+        }
+
+        public anggotaPerpustakaan() {
 
         }
-        public void mencariBuku(){}
+
+        public String getNama() {
+                return nama;
+        }
+
+        public void setNama(String nama) {
+                this.nama = nama;
+        }
+
+        public int getNoAnggota() {
+                return noAnggota;
+        }
+
+        public void setNoAnggota(int noAnggota) {
+                this.noAnggota = noAnggota;
+        }
+
+        public String getAlamat() {
+                return alamat;
+        }
+
+        public void setAlamat(String alamat) {
+                this.alamat = alamat;
+        }
+
+        public ArrayList<transaksiPeminjaman> getRiwayatPeminjaman() {
+                return riwayatPeminjaman;
+        }
+
+        public void setRiwayatPeminjaman(ArrayList<transaksiPeminjaman> riwayatPeminjaman) {
+                this.riwayatPeminjaman = riwayatPeminjaman;
+        }
+
+        public void viewRiwayatPeminjaman(){
+                System.out.println("Riwayat Peminjaman: ");
+                for (transaksiPeminjaman transaksi : riwayatPeminjaman){
+                        System.out.println("ID Transaksi  \t: " + transaksi.getIdTransaksi());
+                        System.out.println("No Anggota\t\t: " + transaksi.getNoAnggota());
+                        System.out.println("No ISBN   \t\t: " + transaksi.getNoISBN());
+                        System.out.println("Tanggal Peminjaman: " + transaksi.getTanggalPeminjaman());
+                        System.out.println("Tenggat Waktu \t: " + transaksi.getTenggatWaktu());
+
+
+                }
+        }
+        public void mencariBuku(String judul){
+                for (transaksiPeminjaman transaksi : riwayatPeminjaman){
+                        if(transaksi.getBuku().getJudul().equals(judul)){
+                                System.out.println("Buku " + judul + " tersedia");
+                                return;
+                        }
+                        System.out.println("Buku " + judul + " Tidak tersedia/sedang dipinjam");
+                }
+        }
         public void terimaNotif(){}
 
 }
--- src/transaksiPeminjaman.java
+++ src/transaksiPeminjaman.java
@@ -1,19 +1,69 @@
-import java.util.List;
 
 public class transaksiPeminjaman extends anggotaPerpustakaan{
     private int idTransaksi;
-    private double noAnggota;
-    private double noISBN;
+    private int noAnggota;
+    private int noISBN;
     private String tanggalPeminjaman;
     private String tenggatWaktu;
     private Buku buku;
 
-    public transaksiPeminjaman(int idTransaksi, double noAnggota, double noISBN, String tanggalPeminjaman, String tenggatWaktu, Buku buku) {
+    public transaksiPeminjaman(int idTransaksi, int noAnggota, int noISBN, String tanggalPeminjaman, String tenggatWaktu, Buku buku) {
+        super();
         this.idTransaksi = idTransaksi;
         this.noAnggota = noAnggota;
         this.noISBN = noISBN;
         this.tanggalPeminjaman = tanggalPeminjaman;
         this.tenggatWaktu = tenggatWaktu;
+        this.buku = buku;
+    }
+
+    public int getIdTransaksi() {
+        return idTransaksi;
+    }
+
+    public void setIdTransaksi(int idTransaksi) {
+        this.idTransaksi = idTransaksi;
+    }
+
+    @Override
+    public int getNoAnggota() {
+        return noAnggota;
+    }
+
+    @Override
+    public void setNoAnggota(int noAnggota) {
+        this.noAnggota = noAnggota;
+    }
+
+    public int getNoISBN() {
+        return noISBN;
+    }
+
+    public void setNoISBN(int noISBN) {
+        this.noISBN = noISBN;
+    }
+
+    public String getTanggalPeminjaman() {
+        return tanggalPeminjaman;
+    }
+
+    public void setTanggalPeminjaman(String tanggalPeminjaman) {
+        this.tanggalPeminjaman = tanggalPeminjaman;
+    }
+
+    public String getTenggatWaktu() {
+        return tenggatWaktu;
+    }
+
+    public void setTenggatWaktu(String tenggatWaktu) {
+        this.tenggatWaktu = tenggatWaktu;
+    }
+
+    public Buku getBuku() {
+        return buku;
+    }
+
+    public void setBuku(Buku buku) {
         this.buku = buku;
     }
 
