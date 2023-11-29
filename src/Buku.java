2e0f945132eddf5e980e18f0ada3a6341635b41d
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2023-11-29 20:40:28 +0700

    Update Buku

--- src/Buku.java
+++ src/Buku.java
@@ -3,4 +3,43 @@ public class Buku {
     private String judul;
     private String noISBN;
     private String statusKetersediaan;
+
+    public Buku(int idBuku, String judul, String noISBN, String statusKetersediaan) {
+        this.idBuku = idBuku;
+        this.judul = judul;
+        this.noISBN = noISBN;
+        this.statusKetersediaan = statusKetersediaan;
+    }
+
+    public int getIdBuku() {
+        return idBuku;
+    }
+
+    public void setIdBuku(int idBuku) {
+        this.idBuku = idBuku;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getNoISBN() {
+        return noISBN;
+    }
+
+    public void setNoISBN(String noISBN) {
+        this.noISBN = noISBN;
+    }
+
+    public String getStatusKetersediaan() {
+        return statusKetersediaan;
+    }
+
+    public void setStatusKetersediaan(String statusKetersediaan) {
+        this.statusKetersediaan = statusKetersediaan;
+    }
 }
