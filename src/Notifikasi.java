b80d8739119d2c02b194f68c2dd887f87e1e4b02
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2023-11-29 20:38:45 +0700

    Update Notifikasi.java

--- src/Notifikasi.java
+++ src/Notifikasi.java
@@ -1,6 +1,59 @@
-public class Notifikasi extends Buku{
+import java.time.LocalDate;
+
+public class Notifikasi{
     private int idNotif;
     private int noAnggota;
     private String pesan;
-    private String timeStamp;
+    private LocalDate timeStamp;
+    private transaksiPeminjaman transaksi;
+
+
+    public Notifikasi(int idNotif, int noAnggota, String pesan, LocalDate timeStamp) {
+        this.idNotif = idNotif;
+        this.noAnggota = noAnggota;
+        this.pesan = pesan;
+        this.timeStamp = timeStamp;
+    }
+
+
+    public int getIdNotif() {
+        return idNotif;
+    }
+
+    public void setIdNotif(int idNotif) {
+        this.idNotif = idNotif;
+    }
+
+    public int getNoAnggota() {
+        return noAnggota;
+    }
+
+    public void setNoAnggota(int noAnggota) {
+        this.noAnggota = noAnggota;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public void setPesan(String pesan) {
+        this.pesan = pesan;
+    }
+
+
+    public LocalDate getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDate timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public transaksiPeminjaman getTransaksi() {
+        return transaksi;
+    }
+
+    public void setTransaksi(transaksiPeminjaman transaksi) {
+        this.transaksi = transaksi;
+    }
 }
